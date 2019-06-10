00eb86effa1966baaed71fdacf89abdda5ff6c8f
Vojtěch Mrkývka <vojtech.mrkyvka@gmail.com>
2019-06-10 10:30:50 +0200

    Added Q/A line prefix variant

--- js/karelparser.js
+++ js/karelparser.js
@@ -9,6 +9,8 @@ export class KarelParser {
     this.counter_a = 0;
 
     let dialogue = [];
+    let mode = null;
+
     // Reading line by line
     text.split(/\r\n|\n/).forEach((line, i) => {
       // Empty line
@@ -18,9 +20,22 @@ export class KarelParser {
       if (line.trim() === '---') {
         this._dialogues.push(dialogue);
         dialogue = [];
+        mode = null;
+        return; // <-- continue
+      }
+
+      if (line.trim() === 'Q:') {
+        mode = 'Q';
+        return; // <-- continue
+      }
+      
+      if (line.trim() === 'A:') {
+        mode = 'A';
+        return; // <-- continue
+      }
 
       // Question
-      } else if (line.trim().startsWith('Q:')) {
+      if (line.trim().startsWith('Q:')) {
         dialogue.push({ part: line.trim().slice('2').trim(), type: 'question' })
         this.counter_q++;
 
@@ -29,6 +44,13 @@ export class KarelParser {
         dialogue.push({ part: line.trim().slice('2').trim(), type: 'answer' })
         this.counter_a++;
 
+      } else if (mode === 'Q') {
+        dialogue.push({ part: line.trim(), type: 'question' })
+        this.counter_q++;
+
+      } else if (mode === 'A') {
+        dialogue.push({ part: line.trim(), type: 'answer' })
+        this.counter_a++;
       // Other - error/undefined
       } else {
         this._errors.push({ part: line, line: i+1 });
